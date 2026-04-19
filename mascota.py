c29a275fc9af04d46de255a34e5da0c2256a6abf
Flavio <flavior.castro.fica@gmail.com>
2026-04-19 19:44:19 -0400

    arreglos

--- mascota.py
+++ mascota.py
@@ -11,6 +11,6 @@ cantidad_vacunas = len(mascota["vacunas"])
 print(f"---|Ficha de la mascota|---")
 print(f"Nombre: {mascota['nombre']}")
 print(f"Especie: {mascota['especie']}")
-print(f"Vacunas registradas: {', '.join(mascota['vacunas'])}")
+print(f"Vacunas registradas: {', '.join(mascota['vacunas'])}") #.join() para mostrar las vacunas en una sola línea separadas por comas.
 print(f"-------------------------------")
 print(f"Total de vacunas: {cantidad_vacunas}")
