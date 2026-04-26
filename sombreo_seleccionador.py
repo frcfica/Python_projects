7fd7859d0f4a3f2387f668d725137302d0a37c8e
Flavio <flavior.castro.fica@gmail.com>
2026-04-26 00:00:13 -0400

    update

--- sombreo_seleccionador.py
+++ sombreo_seleccionador.py
@@ -7,5 +7,45 @@ def sombrero_seleccionador():
     }
 print("==|Bienvenido al sombrero seleccionador|==")
 print("Responde las siguientes preguntas para descubrir a qué casa de Hogwarts perteneces.")
-# Pregunta 1
-print("\nPregunta 1: ¿Cuál es tu cualidad más valorada?")
+preguntas = [
+    {   # Pregunta 1
+        "pregunta": "Como te definiirias mejor?",
+        "opciones": [
+            ("Valiente","Gryffindor"),
+            ("Leal","Hufflepuff"),
+            ("Sabio", "Ravenclaw"),
+            ("Ambicioso", "Slytherin")
+        ]
+    },
+    {   # Pregunta 2
+        "pregunta": "Que animal te gusta mas?",
+        "opciones": [
+            ("Leon", "Gryffindor"),
+            ("Tejon", "Hufflepuff"),
+            ("Aguila", "Ravenclaw"),
+            ("Serpiente", "Slytherin")
+        ]
+    },
+    {   # Pregunta 3
+        "pregunta": "En un duelo de magos, tu...",
+        "opciones": [
+            ("Atacas de frente con coraje", "Gryffindor"),
+            ("Proteges a tus amigos", "Hufflepuff"),
+            ("Usas un hechizo ingeniso y complejo", "Ravenclaw"),
+            ("Haces lo que sea necesario para ganar", "Slytherin")
+        ]
+    },
+    {   # Pregunta 4
+        "pregunta": "Que tipo de magia te atrae mas estudiar?",
+        "opciones": [
+            ("Defensa contra las artes oscuras", "Gryffindor"),
+            ("Herbologia o cuidado de criaturas magicas", "Hufflepuff"),
+            ("Astronomia o runas antiguas", "Ravenclaw"),
+            ("Pociones o artes oscuras", "Slytherin")
+        ]
+    }
+]
+for p in preguntas:
+    print (p["pregunta"])
+    for i, (texto, casa) in enumerate(p["opciones"], 1):
+        print(f"{i}. {texto}")
